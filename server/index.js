c46f87b8f45c98d67bdb11c8c58ada0c6c5c59ac
chloeerazua <chloeerazua@gmail.com>
2021-04-08 20:59:10 -0600

    Edits

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const socketio = require('socket.io'); //decided to use web sockets instead of http request as its a real time app
 const http = require('http');
 const router = require('./router');
+const cors = require('cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
 
@@ -11,7 +12,10 @@ const app = express(); //app initialized from express
 const server = http.createServer(app); //server initialization
 const io = socketio(server); //created an instance of socketio
 
-io.on('connection', (socket) => { //when a socket connects //socket is connected as a client side socket
+app.use(cors());
+app.use(router); //called router as the middleware
+
+io.on('connect', (socket) => { //when a socket connects //socket is connected as a client side socket
     console.log('A user has connected!');
 
     socket.on('join', ({ name, room }, callback) => { //when a user joins
@@ -21,8 +25,8 @@ io.on('connection', (socket) => { //when a socket connects //socket is connected
 
         //if no error 
 
-        socket.emit('message', { user: 'admin', text: '${user.name}, welcome to ${user.room}' }); //system generated message to user that just joined
-        socket.broadcoast.to(user.room).emit('message', { user: 'admin', text: '${user.name}, just joined! '}); //system generated message to other users in the room
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to ${user.room}` }); //system generated message to user that just joined
+        socket.broadcoast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, just joined!`}); //system generated message to other users in the room
 
         socket.join(user.room); //joins user to room
 
@@ -48,7 +52,6 @@ io.on('connection', (socket) => { //when a socket connects //socket is connected
             io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
         }
     })
-})
-app.use(router); //called router as the middleware
+});
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
