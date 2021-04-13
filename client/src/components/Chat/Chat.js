e208a93d6b131202ab74a9d6bed524807d6a7ce1
chloeerazua <chloeerazua@gmail.com>
2021-04-12 20:25:41 -0600

    chat edit

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -52,7 +52,7 @@ const Chat = ({ location }) => {
         }
     }
     
-    // console.log(message, messages);
+    //console.log(messages);
     //create chatbox
     return (
         <div className="outerContainer">
